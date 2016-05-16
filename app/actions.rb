f0afbdb32de5597181506454c36102afdafbb5b4
Dominic Tremblay <dominictremblay24@gmail.com>
2016-05-16 16:47:21 +0000

    step 10 Done

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,4 @@
+
 # Homepage (Root path)
 get '/' do
   erb :index
@@ -8,3 +9,25 @@ get '/messages' do
   erb :'messages/index'
 end
 
+get '/messages/new' do
+  @message = Message.new
+  erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title:   params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
