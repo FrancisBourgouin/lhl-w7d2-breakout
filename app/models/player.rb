963f13018e0363a752922701e85c261708c4f696
Dominic Tremblay <dominictremblay24@gmail.com>
2018-05-22 10:52:30 -0400

    added 2nd uniqueness

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,7 +4,7 @@
     has_many :channels, through: :channel_memberships 
 
     validates :username, presence: true, uniqueness: true, length: {in: 8..15}
-    validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+    validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
     validates :password, presence: true, length: {minimum: 8}
     validates :points, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
